b5a9257e36d5cd98b7071fb9bd2a65f9ef164f73
Jonathan Viegut <viegutj@gmail.com>
2023-08-29 11:08:39 -0500

    commit before merge

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 // CUSTOM COMPONENTS
 import LoginForm from "../LoginForm/LoginForm";
+
+import { Box, Stack} from "@mui/material";
 function LoginPage() {
   return (
     <>
+    <Stack
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      sx={{ width: 1, height: "100vh" }}
+    >
       <LoginForm />
+    </Stack>
     </>
   );
 }
