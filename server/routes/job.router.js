b177943468ca32562e8283e5e5aab1153294f51e
ZakariyeAden <zakariye5417@gmail.com>
2023-08-29 15:08:56 -0500

    set up for POST router

--- server/routes/job.router.js
+++ server/routes/job.router.js
@@ -1,21 +1,66 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const {
   rejectUnauthenticated,
-} = require('../modules/authentication-middleware');
+} = require("../modules/authentication-middleware");
 const router = express.Router();
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
-  // GET route code here
+
+router.get("/", (req, res) => {
+  // GET route
 });
 
-/**
- * POST route template
- */
-router.post('/', (req, res) => {
-  // POST route code here
+// POST for Benefits and Jobs info
+router.post("/", async (req, res) => {
+  let jobInfo = req.body;
+  let benefitInfo = req.body;
+  // Parameterizations
+  let paramsJobInfo = [
+    jobInfo.userId,
+    jobInfo.jobTitle,
+    jobInfo.jobRole,
+    jobInfo.Company,
+    jobInfo.state,
+    jobInfo.promotions,
+    jobInfo.jobNumber,
+    jobInfo.hours,
+    jobInfo.jobDuration,
+    jobInfo.jobType,
+    jobInfo.dateHired,
+    jobInfo.salary,
+    jobInfo.graduationDate,
+    jobInfo.bootcamp,
+  ];
+  let paramsBenefitInfo = [
+    benefitInfo.health,
+    benefitInfo.dental,
+    benefitInfo.PTO,
+    benefitInfo.fourZeroOneK,
+    benefitInfo.equity,
+    benefitInfo.bonuses,
+    benefitInfo.LTD,
+    benefitInfo.STD,
+    benefitInfo.notes,
+  ];
+  // We need to use the same connection for all queries...
+  const connection = await pool.connect();
+
+  try {
+    await connection.query("BEGIN");
+    const queryJobText = `INSERT INTO "job_info" ("user_id","job_title", "job_role", "company", "state", "promotions", "job_number","hours", "job_duration", "job_type", "date_hired", "salary", "graduation_date","bootcamp")
+  VALUES ($1,$2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14);`;
+    const queryBenefitText = `INSERT INTO "benefits" ("health_insurance","dental_insurance","PTO", "401K", "equity", "bonuses", "long_term_disability","short_term_disability", "notes")
+  VALUES ( $1, $2, $3, $4, $5, $6, $7, $8, $9);`;
+    await connection.query(queryJobText, paramsJobInfo);
+    await connection.query(queryBenefitText, paramsBenefitInfo);
+    await connection.query("COMMIT");
+    res.sendStatus(200);
+  } catch (err) {
+    await connection.query("ROLLBACK");
+    console.log(`ERROR in POST for job and benefits:`, err);
+    res.sendStatus(500);
+  } finally {
+    connection.release();
+  }
 });
 
 module.exports = router;
