a5480951a3837f3d8152dcffd496773859c25a91
ZakariyeAden <zakariye5417@gmail.com>
2023-09-09 15:15:25 -0500

    fixing bug for Registers directing for Home

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -74,7 +74,7 @@ function App() {
             {user.id ? (
               // If the user is already logged in,
               // redirect them to the /user page
-              <Redirect to="/user" />
+              <Redirect to="/home" />
             ) : (
               // Otherwise, show the registration page
               <RegisterPage />
